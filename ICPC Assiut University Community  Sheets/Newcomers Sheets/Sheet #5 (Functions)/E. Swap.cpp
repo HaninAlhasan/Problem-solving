9e56100fb76ab1dbebd2e05646477bf54f107277
Haneen Ahmed Alhasan <haneenalhasan123@gmail.com>
2022-12-11 22:05:19 +0200

    Create E. Swap.cpp

---
+++ ICPC Assiut University Community  Sheets/Newcomers Sheets/Sheet #5 (Functions)/E. Swap.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool swap(int x,int y);
+int main()
+{
+    int x,y;
+    cin >> x >> y;
+    swap(x,y);
+    
+	return 0;
+}
+bool swap(int x,int y)
+{
+    cout << y << " " <<  x << endl;
+    return 0;
+}
